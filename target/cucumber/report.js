db243f0a356a9ad2d72f453dbc9ef3bf37e93daf
nisha <nisha.shinde@candytechnologies.in>
2020-11-05 00:01:15 +0530

    add some changes in pom pages

---
+++ target/cucumber/report.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:/home/nisha/master_syncoffice/Feature_file/Cloud_feature");
+formatter.feature({
+  "name": "To Create a folder using Header Option Icon",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To Create a folder - Header Option",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "I launch chrome browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Master_stepdefinition.i_launch_chrome_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I open Url \"https://www.syncoffice.com/module-test-dashboard/dashboard\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Master_stepdefinition.i_open_Url(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I Click on login button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Master_stepdefinition.i_Click_on_login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I enter UserName \"sarika.mishra@candytechnologies.in\" And Password \"123456\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Master_stepdefinition.i_enter_UserName_And_Password(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I Click on Login",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Master_stepdefinition.i_Click_on_Login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Cloud",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Master_stepdefinition.click_on_Cloud()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on My Cloud",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Master_stepdefinition.click_on_My_Cloud()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Create Folder Icon",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Master_stepdefinition.click_on_Create_Folder_Icon()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Create folder pop up opens Name a new Folder",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Master_stepdefinition.create_folder_pop_up_opens_Name_a_new_Folder()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click On Create Button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Master_stepdefinition.click_On_Create_Button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Verify",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Master_stepdefinition.verify()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
